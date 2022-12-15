21bac16d5c504046702a6d25ad9be70cc93b13d1
alexis fritsch <arckanics.reiko@gmail.com>
2022-12-15 14:33:38 +0100

    trying some update

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
-import '../scss/styles.scss'
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
+
+
+console.log(bootstrap.ScrollSpy);
+
+const dataSpyList = document.querySelectorAll('[data-bs-spy="scroll"]')
+
+console.log(dataSpyList);
+
+dataSpyList.forEach(dataSpyEl => {
+  let instance = bootstrap.ScrollSpy.getInstance(dataSpyEl)
+  console.log(instance)
+})
