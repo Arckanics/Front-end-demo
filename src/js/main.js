37b350d774da5c8b6a8f2d401cba058da94a5327
alexis fritsch <arckanics.reiko@gmail.com>
2022-12-15 17:56:48 +0100

    scroll fixing

--- src/js/main.js
+++ src/js/main.js
@@ -8,22 +8,20 @@ const nav = {
   items: [...document.getElementById("site-nav").querySelectorAll('.nav-link')],
   spy: new ScrollSpy(document.body, {
     target: "#site-nav"
-  }),
-  offset: () => {
-    return document.querySelector("#site-nav").getBoundingClientRect().height
-  }
+  })
 }
 
-nav.items.map(i => {
-  i.onclick = (e) => {
-    let target = document.querySelector(i.getAttribute('href'))
-    let scrollPos = target.offsetTop - nav.offset()
-
-    console.log(target.getBoundingClientRect());
-
-    nav.root.parentNode.scrollTo({
-      top: scrollPos,
-      behavior: 'smooth'
-    })
-  }
-})
+  nav.items.map(i => {
+    i.onclick = null
+    i.onclick = (e) => {
+      let target = document.querySelector(i.getAttribute('href'))
+      let scrollPos = target.offsetTop - 80
+  
+      console.log(target.getBoundingClientRect());
+  
+      nav.root.parentNode.scrollTo({
+        top: scrollPos,
+        behavior: 'smooth'
+      })
+    }
+  })
